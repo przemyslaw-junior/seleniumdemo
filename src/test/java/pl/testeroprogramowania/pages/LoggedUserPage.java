d2ad95b34a3f22d7c31d190d602ef64e8dd06441
Przemek T <przemek8319@gmail.com>
2022-03-25 18:50:33 +0100

    create Logger User Page create Register Test

--- src/test/java/pl/testeroprogramowania/pages/LoggedUserPage.java
+++ src/test/java/pl/testeroprogramowania/pages/LoggedUserPage.java
@@ -1,8 +1,23 @@
 package pl.testeroprogramowania.pages;
 
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
 
 public class LoggedUserPage {
+
+    @FindBy(xpath = "//h1[@class='entry-title']")
+    private WebElement entryTitle;
+
+    public WebDriver driver;
+
     public LoggedUserPage(WebDriver driver) {
+        PageFactory.initElements(driver, this);
+        this.driver = driver;
+    }
+
+    public WebElement getEntryTitle(){
+        return entryTitle;
     }
 }
--- src/test/java/pl/testeroprogramowania/tests/RegisterTest.java
+++ src/test/java/pl/testeroprogramowania/tests/RegisterTest.java
@@ -1,5 +1,7 @@
 package pl.testeroprogramowania.tests;
 
+import org.openqa.selenium.WebElement;
+import org.testng.Assert;
 import org.testng.annotations.Test;
 import pl.testeroprogramowania.pages.HomePage;
 
@@ -7,7 +9,12 @@ public class RegisterTest extends BaseTest {
 
     @Test
     public void registerUserTest() {
-        new HomePage(driver).openMyAccountPage()
-                .registerUser("juniortest@gmail.com", "juniortest@gmail.com" );
+        WebElement entryTitle = new HomePage(driver)
+                .openMyAccountPage()
+                .registerUser("juniortest1@gmail.com", "juniortest1@gmail.com" )
+                .getEntryTitle();
+
+        Assert.assertTrue(entryTitle.isDisplayed());
+        Assert.assertEquals(entryTitle.getText(), "My account");
     }
 }
