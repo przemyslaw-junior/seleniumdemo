5446edaad60d989f6d2dc69a6e18eb9d45dc3c74
Przemek T <przemek8319@gmail.com>
2022-03-28 19:11:01 +0200

    add Address Details Page

--- src/test/java/pl/testeroprogramowania/pages/AddressDetailsPage.java
+++ src/test/java/pl/testeroprogramowania/pages/AddressDetailsPage.java
@@ -1,10 +1,55 @@
 package pl.testeroprogramowania.pages;
 
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
 
 public class AddressDetailsPage {
 
+    private WebDriver driver;
+
+    @FindBy(id = "billing_first_name")
+    private WebElement firstNameInput;
+
+    @FindBy(id = "billing_last_name")
+    private WebElement lastNameInput;
+
+    @FindBy(id = "billing_company")
+    private WebElement companyNameInput;
+
+    @FindBy(id = "select2-billing_country-container")
+    private WebElement billingCountryInput;
+
+    @FindBy(id = "billing_address_1")
+    private WebElement billingAddressInput;
+
+    @FindBy(id = "billing_address_2")
+    private WebElement billingAddressSecondInput;
+
+    @FindBy(id = "billing_postcode")
+    private WebElement billingPostcodeInput;
+
+    @FindBy(id = "billing_postcode")
+    private WebElement billingCityInput;
+
+    @FindBy(id = "billing_phone")
+    private WebElement billingPhoneInput;
+
+    @FindBy(id = "billing_email")
+    private WebElement billingEmailInput;
+
+    @FindBy(id = "order_comments")
+    private WebElement orderCommentsInput;
+
+    @FindBy(id = "place_order")
+    private WebElement placeOrderButton;
+
     public AddressDetailsPage(WebDriver driver) {
 
+        PageFactory.initElements(driver, this);
+        this.driver = driver;
     }
+
+
 }
